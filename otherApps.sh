9ddb799e42c359ba4ac4f8c801d3a7f0a451b80f
SYKhayyat <shaulyoelkhayyat@gmail.com>
2026-02-01 13:36:39 -0500

    Added otherApps

---
+++ otherApps.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# This file is for you!
+# There are some apps which cannot be installed in the declarative way 
+# (because they are not in the regular repository). 
+# This file is for you to keep track of your software, 
+# so that you will easily be able to restore your machine to its desired state.
